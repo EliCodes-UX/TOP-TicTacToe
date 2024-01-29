7ae60597ce35bd714b7f0bfbd72a4319904d1266
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-01-28 21:00:16 -0700

    added gameboard function to script

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-const gameBoard = () => {};
+const board = document.querySelector('.gameBoard');
+
+const gameBoard = (() => {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push('');
+    }
+  }
+  const getBoard = () => board;
+  return { getBoard };
+})();
+console.log(gameBoard.getBoard());

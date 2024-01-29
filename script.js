7fc6432761e38f499dd41f65593d35580fb03c7b
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-01-28 21:57:40 -0700

    added more js to game controller functions

--- script.js
+++ script.js
@@ -25,6 +25,47 @@ const createCells = () => {
   }
 };
 createCells();
-// const controller = () = {
 
-// }
+function GameController(
+  playerOneName = 'Player One',
+  playerTwoName = 'Player Two'
+) {
+  const players = [
+    {
+      name: playerOneName,
+      mark: 'X',
+    },
+    {
+      name: playerTwoName,
+      mark: 'O',
+    },
+  ];
+  let running = false;
+  let activePlayer = players[0];
+
+  const winner = document.querySelector('.winner');
+  const modal = document.querySelector('.result');
+  const board = gameBoard.getBoard();
+  const playerOne = document.querySelector('.player1');
+  const playerTwo = document.querySelector('.player2');
+  const resetBtn = document.querySelector('.reset');
+
+  const changePlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    if (activePlayer === players[0]) {
+      playerTwo.removeAttribute('active');
+      playerOne.setAttribute('active', 'activePlayer');
+    } else if (activePlayer === players[1]) {
+      playerOne.removeAttribute('active');
+      playerTwo.setAttribute('active', 'activePlayer');
+    }
+  };
+  const playersTurn = () => activePlayer;
+
+  const plaRound = () => {
+    cells.forEach(cell => cell.addEventListener('click', clickedCell));
+    resetBtn.addEventListener('click', restartGame);
+    gameText.textContent = `${currentPlayer}'s turn`;
+    running = true;
+  };
+}
